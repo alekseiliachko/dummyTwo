bd0b273a811ef660a47346ebe1448383bc11a8a1
Aleksey Lyachko <40854818+alekseiliachko@users.noreply.github.com>
2021-04-14 18:05:07 +0300

    initial

---
+++ src/main/java/com/example/dummyTwo/DummyTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dummyTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dummyTwo/DummyTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dummyTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
